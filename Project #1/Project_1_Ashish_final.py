62a1fd7cdd82fb36e69c6c1c07cb6f6beb2f91bb
JacobAlbus <jacobalbus20@gmail.com>
2023-10-16 16:42:02 -0500

    Benchmark Code

--- Project #1/Project_1_Ashish_final.py
+++ Project #1/Project_1_Ashish_final.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Import modules
 import pandas as pd
 import numpy as np
-import xgboost as xgb
-import time
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LassoCV, RidgeCV, ElasticNetCV
 
+random_seed = 42
+np.random.seed(random_seed)
+
+
+# In[2]:
+
+
+#Load and preprocess data
 def lasso_preprocess_data(data, train_columns=None, up_quantile = None):
     #Winsorize features
     winsor_features = ["Lot_Frontage", "Lot_Area", "Mas_Vnr_Area", "BsmtFin_SF_2", "Bsmt_Unf_SF", 
@@ -46,10 +58,13 @@ def lasso_preprocess_data(data, train_columns=None, up_quantile = None):
         return data, up_quantile
 
 
-def lasso_fit_and_predict(i):
-    train_data = pd.read_csv(f'data/fold{i}/train.csv')
-    test_data = pd.read_csv(f'data/fold{i}/test.csv')
-    y_test = pd.read_csv(f'data/fold{i}/test_y.csv')
+# In[3]:
+
+
+def lasso_fit_and_predict():
+    train_data = pd.read_csv('train.csv')
+    test_data = pd.read_csv('test.csv')
+    y_test= pd.read_csv('test_y.csv')
 
     # Dropping outliers as suggested by data documentation page, taking a more aggressive approach i.e. 3500 limit vs 4000
     train_data = train_data[train_data['Gr_Liv_Area'] <= 3800]
@@ -84,7 +99,7 @@ def lasso_fit_and_predict(i):
     elastic_pred = model_elastic.predict(x_test_pp)
     elastic_rmse = np.sqrt(mean_squared_error(elastic_pred, y_test))
 
-    print(f"ElasticNet {i} RMSE", elastic_rmse)
+    print("ElasticNet RMSE", elastic_rmse)
     
     #Save predictions
     submission_df = pd.DataFrame({
@@ -94,69 +109,4 @@ def lasso_fit_and_predict(i):
     
     submission_df.to_csv('mysubmission1.txt', index=False)
 
-
-def preprocess_xgb_features(data, train_columns=None):
-    # Some values in Mas_Vnr_Type and Misc_feature have a Nan float value instead of just a "None" string
-    data = data.fillna('None')
-    # Some homes don't have garages so replace their "None" string with 0
-    data["Garage_Yr_Blt"] = data["Garage_Yr_Blt"].replace("None", 0)
-
-    try:
-        y = np.log(data["Sale_Price"])
-    except KeyError:
-        y = None
-
-    # Select all features except Sales Price
-    best_features = list(data.columns)[:-1]
-    data = data[best_features]
-
-    # One hot encoding of nominal features
-    X = pd.get_dummies(data[best_features])
-
-    # Handle column mismatch from one hot encoding
-    if train_columns is not None:
-        missing_columns = set(train_columns) - set(X.columns)
-        for column in missing_columns:
-            X[column] = 0
-        # Ensure the order of column in the test set is in the same order than in train set
-        X = X[train_columns]
-
-    return X, y
-
-def train_xgb_model(i):
-  X_train, y_train = preprocess_xgb_features(pd.read_csv(f"data/fold{i}/train.csv"))
-
-  cv_eta = 0.025
-  cv_T = 10000
-
-  X_test, _ = preprocess_xgb_features(pd.read_csv(f"data/fold{i}/test.csv"), X_train.columns)  
-
-  clf = xgb.XGBRegressor(n_estimators=cv_T, learning_rate=cv_eta, 
-                        max_depth=6, subsample=0.5, tree_method="exact")
-  clf.fit(X_train, y_train)
-  yhat = clf.predict(X_test)
-
-
-  y = np.log(pd.read_csv(f"data/fold{i}/test_y.csv")["Sale_Price"])
-  elastic_rmse = np.sqrt(mean_squared_error(yhat, y))
-  print(f"XGB {i} RMSE", elastic_rmse)
-
-  output = pd.DataFrame({"PID" : X_test["PID"], "Sale_Price" : np.exp(yhat) })
-  output.to_csv("mysubmission2.txt", index=False)
-
-for i in range(1,11):
-    print(f"=== FOLD {i} ===")
-    start_time = time.time()
-    lasso_fit_and_predict(i)
-
-    print("Elastinet runtime:", time.time() - start_time)
-    start_time = time.time()
-
-    train_xgb_model(i)
-
-    print("XGBoost runtime:", time.time() - start_time)
-
-# y_hat = np.log(pd.read_csv("mysubmission1.txt")["Sale_Price"])
-# y = np.log(pd.read_csv("test_y.csv")["Sale_Price"])
-
-# print(np.sqrt(np.mean((y_hat - y)**2)))
+lasso_fit_and_predict()
